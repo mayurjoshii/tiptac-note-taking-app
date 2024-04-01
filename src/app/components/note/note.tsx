f9a5b5f6097216f83a873ba969002e9befbb7cd7
mayurjoshii <mayur97joshi@yahoo.com>
2024-04-01 23:25:27 +0530

    Change

--- src/app/components/note/note.tsx
+++ src/app/components/note/note.tsx
@@ -2,12 +2,6 @@ import React from 'react'
 
 
 export const Note = () => {
-    // const { noteId } = useParams<{
-    //     noteId?: string
-    // }>()
-    // console.log({
-    //     noteId
-    // })
     return (
         <div>
             I am new note
--- src/app/containers/home/index.tsx
+++ src/app/containers/home/index.tsx
@@ -2,51 +2,9 @@ import React from 'react';
 import style from './style.css';
 import { Header } from 'app/components/header';
 import { Main, Sidebar } from 'app/components';
-import { useSelector } from 'react-redux';
-import { RootState } from 'app/reducers';
 
-// const FILTER_VALUES = (Object.keys(TodoModel.Filter) as (keyof typeof TodoModel.Filter)[]).map(
-//   (key) => TodoModel.Filter[key]
-// );
-
-// const FILTER_FUNCTIONS: Record<TodoModel.Filter, (todo: TodoModel) => boolean> = {
-//   [TodoModel.Filter.SHOW_ALL]: () => true,
-//   [TodoModel.Filter.SHOW_ACTIVE]: (todo) => !todo.completed,
-//   [TodoModel.Filter.SHOW_COMPLETED]: (todo) => todo.completed
-// };
 
 export const Home = () => {
-  // const dispatch = useDispatch();
-  // const todoActions = useTodoActions(dispatch);
-  // const { todos, filter } = useSelector((state: RootState) => {
-  //   const hash = location?.hash?.replace('#', '');
-  //   return {
-  //     todos: state.todos,
-  //     filter: FILTER_VALUES.find((value) => value === hash) ?? TodoModel.Filter.SHOW_ALL
-  //   };
-  // });
-
-  // const handleClearCompleted = React.useCallback((): void => {
-  //   todoActions.clearCompleted();
-  // }, [todoActions]);
-
-  // const handleFilterChange = React.useCallback(
-  //   (filter: TodoModel.Filter): void => {
-  //     history.push(`#${filter}`);
-  //   },
-  //   [history]
-  // );
-
-  // const filteredTodos = React.useMemo(() => (filter ? todos.filter(FILTER_FUNCTIONS[filter]) : todos), [todos, filter]);
-  // const activeCount = React.useMemo(() => todos.filter((todo) => !todo.completed).length, [todos]);
-  // const completedCount = React.useMemo(() => todos.filter((todo) => todo.completed).length, [todos]);
-
-  const currentNotes = useSelector((state: RootState) => {
-    console.log("Root state--", state)
-    return state.notes
-  })
-
-  console.log("Current notes--", currentNotes)
 
   return (
     <div className={style.container}>
