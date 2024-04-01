4565f2cbc7252068db7f9e9b35666b3ad1d78ac9
mayurjoshii <mayur97joshi@yahoo.com>
2024-04-01 23:50:06 +0530

    Removed header

--- src/app/containers/home/index.tsx
+++ src/app/containers/home/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import style from './style.css';
-import { Header, Main, Sidebar } from 'app/components';
+import { Main, Sidebar } from 'app/components';
 export const Home = () => {
 
   return (
     <div className={style.container}>
-      <Header />
       <div className={style.sidebarAndMainContainer}>
         <Sidebar />
         <Main />
