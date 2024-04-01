10456ae31748575c589176ed4880954c7cdd60fc
mayurjoshii <mayur97joshi@yahoo.com>
2024-04-01 23:27:47 +0530

    Header rename

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './header.css'
+
+export const Header = () => {
+    return (
+        <div className={style.container}>
+            <h3>Note taking app</h3>
+        </div>
+    )
+}
